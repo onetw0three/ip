e5d7a96ee87de896c70acba20b963e397776d8f0
Tan Ze Jian <110755344+onetw0three@users.noreply.github.com>
2026-01-16 22:46:11 +0800

    Add echo

--- src/main/java/Huhhh.java
+++ src/main/java/Huhhh.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+
 public class Huhhh {
+    private static String formatMessage(String msg) {
+        return String.format(
+            "    ____________________________________________________________\n" +
+            "     %s\n" +
+            "    ____________________________________________________________\n",
+                msg.replace("\n", "\n     ")
+            );
+    }
+
+    private static void greet() {
+        String welcomeMsg = "Hello! I'm Huhhh\nWhat can I do for you?";
+        System.out.println(formatMessage(welcomeMsg));
+    }
+
+    private static void echo(String msg) {
+        System.out.println(formatMessage(msg));
+    }
+
+    private static void goodbye() {
+        System.out.println(formatMessage("Bye. Hope to see you again soon!"));
+    }
+
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Huhhh");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        greet();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                goodbye();
+                break;
+            }
+            echo(input);
+        }
     }
 }
